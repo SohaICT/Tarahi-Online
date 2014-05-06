06e6d01154c4c397aa8ac180a135a643be2eb0be
alinazari7092 <ali_nazari7092@yahoo.com>
2014-05-06 14:27:32 +0430

    get_request_product in model(request) added

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -4,6 +4,24 @@ class Test extends CI_Controller
 	public function index()
 	{
 		$this->load->model('request');
-		$data=$request->get_user_open_request(1);
+		$data=$this->request->get_user_open_requests(2);
+		foreach ($data as $x)
+		{
+			echo $x['RID'];
+			echo $x['DID'];
+			echo $x['status'];
+			echo $x['designer_name'];
+		}
+	}
+	public function test1()
+	{
+		$this->load->model('request');
+		$data=$this->request->get_request_products(10);
+		foreach ($data as $x)
+		{
+			echo $x['product_ID'];
+			echo $x['preview'];
+			echo $x['file_name'];
+		}
 	}
 }
--- application/models/request.php
+++ application/models/request.php
@@ -11,11 +11,22 @@ class Request extends CI_Model
 	//OUTPUT: (num_rows() > 0): $array[i][RID, DID, status, designer_name] else false
 	{
 		$names = array('c');
-		$this->db->select('RID , DID , status , name as designer_name');
+		$this->db->select('RID , request.DID , status , name as designer_name');
 		$this->db->from('request');
 		$this->db->join('designer','designer.DID = request.DID');
 		$this->db->where('UID',$uid);
 		$this->db->where_not_in('status', $names);
+		$query = $this->db->get();
+		if($query->num_rows() == 0)
+			return false;
+		return $query->result_array();
+	}//End of get_user_open_request()
+	public function get_request_products($rid)
+	//OUTPUT: (num_rows() > 0): $array[i][productID,preview,filename] else false
+	{
+		$this->db->select('product_ID , preview , file_name , RID');
+		$this->db->from('product');
+		$this->db->where('RID',$rid);
 		$query = $this->db->get();
 		if($query->num_rows() == 0)
 			return false;
